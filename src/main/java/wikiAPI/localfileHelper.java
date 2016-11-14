2e4edc37d1a1f2413355881011447d83064e2ff0
rene2u9 <rene2u9@gmail.com>
2016-11-14 09:16:29 +0100

    getting file size more efficient checking max cores definition generation coverage percentage added

--- src/main/java/wikiAPI/localfileHelper.java
+++ src/main/java/wikiAPI/localfileHelper.java
@@ -4,30 +4,23 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.*;
 import java.util.ArrayList;
-
 import static java.lang.StrictMath.round;
 
-
 class localfileHelper extends Thread {
-
-    private int max;
-    private RandomAccessFile file;
     private int id,
-            chunksize,
-            pages = 0,
-            definitions = 0,
-            errors = 0;
+                max,
+                pages = 0,
+                definitions = 0,
+                errors = 0;
+    private long chunksize;
+    private RandomAccessFile file;
 
     localfileHelper(String path, int threadid, int threadcount, int max) throws IOException {
         this.id = threadid;
         this.max = max;
         try {
-            //gehts sicherlich besser
-            FileInputStream inputStream = new FileInputStream(path);
-            this.chunksize = (inputStream.available() / threadcount);
-            inputStream.close();
-
             file = new RandomAccessFile(path, "r");
+            this.chunksize =  (file.getChannel().size() / threadcount);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -49,85 +42,89 @@ class localfileHelper extends Thread {
                 pos = file.getChannel().position();
                 //threadprocess
                 //System.out.println(threadnr+" : " + pos+ " " + chunksize*threadnr);
-                if ((line = file.readLine()) == (null) || pos >= stop) {
-                    System.out.println();
-                    break;
-                }
+                if ((line = file.readLine()) == (null) || pos >= stop)  break;
                 if (line.contains("<page id=")) {
                     pages++;
-                    if (pages == 1) {
-                        start = file.getChannel().position();
-                    }
+                    if (pages == 1) start = file.getChannel().position();
                     if (pages > max) {
                         stoppingreason = "max sites parsed!";
                         break;
                     }
-
                     line = file.readLine();
                     while (!line.contains("</page>")) {
                         article += line + "\n";
                         line = file.readLine();
                     }
-
                     //TODO: create definition and store to db?
                     getDefinition(article);
                     //xmlHelper xml= new xmlHelper();
                     //System.out.println(xml.getTagValue(article, "title") + " : " + getDefinition(article));
-
                     article = "";
                 }
             }
             file.close();
             //Threadstats
             printStats(stoppingreason, System.currentTimeMillis() - startTime, start, pos);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private String getDefinition(String article) {
-        //TODO: absatz besser extrahieren
-        String definition = "";
-        try {
-            definition = article.substring(0, article.indexOf("\n\n"));
-        } catch (StringIndexOutOfBoundsException e) {
-            errors++;
+    private String getDefinition(String article) throws Exception {
+        //TODO: absatz besser extrahieren, encoding...
+        String definition;
+        definition = new String(article.getBytes("UTF-8"));
+        //cut definition
+        if (article.indexOf("\n\n") > 0) {
+            definition = definition.substring(0, article.indexOf("\n\n"));
         }
         Document doc = Jsoup.parse(definition);
-        definition = doc.select("text").text()
-                .replaceAll("(\\[http://)([^\\[\\]]*)(\\])", "")
+        definition = doc.select("text").text().trim()
+                //        .replaceAll("(\\[http://)([^\\[\\]]*)(\\])", "")
                 .replaceAll("\\[ \\]", "")
-                .replaceAll("\\( \\)", "");
+                .replaceAll("\\( \\)", "")
+                .replaceAll(" ; ", "")
+                //wtf?! wozu benutz ich denn einen parser...
+                .replaceAll("Ã¼", "ü")
+                .replaceAll("Ã¤", "ä")
+                .replaceAll("Ã¶", "ö")
+                .replaceAll("Ã\u009F", "ß")
+        ;
         //blacklist
         if (definition.length() < 50 || definition.length() > 2000) {
             errors++;
             return "";
         }
         definitions++;
-        return definition;
+        return new String(definition.getBytes("UTF-8"));
     }
 
     private void printStats(String reason, long time, long start, long stop) {
         System.out.println(
-                "\nThread" + id + " stopped:\t"+reason+
-                        "\nChunk (in bytes):\t" + start + " - " + stop +
-                        "\nTime (in millis):\t" + time +
-                        "\nProcessed pages:\t" + (definitions + errors) + " / " + pages +
-                        "\nsucced / failed:\t" + definitions + " / " + errors +
-                        "\t(" + round((definitions/(pages*1.)*100.)) +"%)"
-            );
+                        "\nThread" + id + " stopped:\t" + reason +
+                        "\n" + round((stop - start)/1000.) + "kB from:\t\t" + start + " to " + stop +
+                        "\nTime (in millis):\t" + time + " (" + round(time/1000.) + " s)"+
+                        "\nProcessed pages:\t" + (definitions + errors) + " (" + pages + "found)"+
+                        "\nDefGen coverage:\t" + round((definitions / (pages * 1.) * 100.)) +
+                        "% (+" + definitions + "/ -" + errors + ")"
+                        );
     }
 }
 
 
 class filetest {
     public static void main(String[] args) throws IOException {
-        int threadnr = 1;
-        int maxpages = 500;
+        int threadnr = 4;
+        int maxpages = 100;
+        if(threadnr >Runtime.getRuntime().availableProcessors()){
+            threadnr=Runtime.getRuntime().availableProcessors();
+        }
 
+        String path = "C:\\Users\\rene2\\Desktop\\dewiki.xml";
+        //spawns and starts threads
         ArrayList<localfileHelper> threads = new ArrayList<>();
         for (int i = 0; i < threadnr; i++) {
-            threads.add(new localfileHelper("C:\\Users\\rene2\\Desktop\\dewiki.xml", i, threadnr, maxpages / threadnr));
+            threads.add(new localfileHelper(path, i, threadnr, maxpages / threadnr));
         }
         for (localfileHelper thread : threads) {
             thread.start();
