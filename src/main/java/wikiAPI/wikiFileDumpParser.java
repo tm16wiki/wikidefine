9e5d050cb67b165346cdfc6e6881284601abb118
rene2u9 <rene2u9@gmail.com>
2016-12-15 16:11:21 +0100

    documented and shortened

--- src/main/java/wikiAPI/wikiFileDumpParser.java
+++ src/main/java/wikiAPI/wikiFileDumpParser.java
@@ -1,15 +1,15 @@
 package wikiAPI;
 
 import helperClasses.xml;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-
 import static java.lang.StrictMath.round;
 import static java.lang.Thread.sleep;
 
-
+/**
+ * class for processing wikipedia xml dump as file
+ */
 public class wikiFileDumpParser {
     private helperClasses.db db;
     private int pages = 0,
@@ -22,27 +22,38 @@ public class wikiFileDumpParser {
             startTime;
     private boolean verbose = false;
 
-
-    public wikiFileDumpParser(int threadnr, int maxpages, boolean printstats, boolean verbose, String path, helperClasses.db db) {
-        if (threadnr > Runtime.getRuntime().availableProcessors()) {
-            threadnr = Runtime.getRuntime().availableProcessors();
+    /**
+     * constructor for setting up parameter, spawn and run processthreads and wait for them
+     * @param threads number of threads to spawn
+     * @param maximum maximum of pages to process
+     * @param printstats boolean print statistics
+     * @param verbose boolean verbose
+     * @param path path to file
+     * @param db database to store
+     */
+    public wikiFileDumpParser(int threads, int maximum, boolean printstats, boolean verbose, String path, helperClasses.db db) {
+        if (threads > Runtime.getRuntime().availableProcessors()) {
+            threads = Runtime.getRuntime().availableProcessors();
         }
         this.verbose = verbose;
         this.db = db;
-        this.max = maxpages;
-        this.threadcount = threadnr;
+        this.max = maximum;
+        this.threadcount = threads;
         try {
             this.startTime = System.currentTimeMillis();
-            this.chunksize = new RandomAccessFile(path, "r").getChannel().size() / threadnr;
+            this.chunksize = new RandomAccessFile(path, "r").getChannel().size() / threads;
 
             fileThread thread = null;
-            for (int i = 0; i < threadnr; i++) {
+            for (int i = 0; i < threads; i++) {
                 thread = new fileThread(new RandomAccessFile(path, "r"), i);
                 thread.start();
             }
+            //synchronize over last thread
             synchronized (thread) {
                 try {
+                    //wait for last thread to finish
                     thread.wait();
+                    //wait some time to finish the printprocess
                     sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -58,11 +69,19 @@ public class wikiFileDumpParser {
         }
     }
 
+    /**
+     * prints results and time needed
+     * @param starttime starttime
+     */
     public void stats(long starttime) {
+        //calculate time needed
         long time = System.currentTimeMillis() - starttime - 100;
-        System.out.println("finished after " + round(time / 1000.) + " s." +
-                "\n" + round((definitions / ((definitions + errors) * 1.) * 100.)) + "% ( +" + definitions + " / -" + errors + " )" +
-                " prefiltered: " + prefiltered);
+        //print information
+        System.out.println(
+                "finished after " + round(time / 1000.) + " s.\n" +
+                round((definitions / ((definitions + errors) * 1.) * 100.)) + "% ( +" + definitions + " / -" + errors + " )" +
+                " prefiltered: " + prefiltered
+        );
     }
 
 
@@ -70,36 +89,39 @@ public class wikiFileDumpParser {
         private int id;
         private RandomAccessFile file;
 
+        /**
+         * constructor for processing thread
+         * @param file file to process
+         * @param threadid id of the thread
+         */
         fileThread(RandomAccessFile file, int threadid) {
             this.id = threadid;
             this.file = file;
         }
 
+
+        /**
+         * processes filechunk generates definition and stores it to db
+         */
         @Override
         public synchronized void run() {
             xml xml = new xml();
             wikiTextParser text = new wikiTextParser();
-            String page = "",
-                    line;
-
+            String page = "", line;
             try {
                 //seek to chunkstart
                 file.seek(chunksize * (id));
-
-                long pos;
-                while (true) {
+                long pos = file.getChannel().position();
+                while ((line = file.readLine()) != (null) && pos < chunksize * (id + 1) && pages-prefiltered<=max) {
                     pos = file.getChannel().position();
-                    if ((line = file.readLine()) == (null) || pos >= chunksize * (id + 1))
-                        break;
-                    //if (line.contains("<page id=")) { //for preprocessed pages
-                    if (line.contains("<page>")) {
+                    if (line.contains("<page>")) { //line.contains("<page id=") { //for preprocessed pages
                         pages++;
-                        if (pages - prefiltered > max) break;
-                        //own implementation...
                         line = readLineUTF();
                         boolean reject = false;
-                        while (!line.contains("</page>") & !(line == null)) {
-
+                        while (!line.contains("</page>") ) { //&
+                            if(line == null){
+                                break;
+                            }
                             //TODO: prefilter for lists or redirects
                             if (line.contains("#REDIRECT") ||
                                     line.contains("#WEITERLEITUNG") ||
@@ -108,12 +130,10 @@ public class wikiFileDumpParser {
                                     line.contains("#redirect") ||
                                     line.contains("#Redirect")
                                     ) {
-                                page += "FILTERED</text>";
                                 prefiltered++;
                                 reject = true;
                                 break;
                             }
-
                             page += line + "\n";
                             //line = file.readLine();
                             line = readLineUTF();
@@ -122,7 +142,6 @@ public class wikiFileDumpParser {
                             String article = xml.getTagValue(page, "text");
                             String title = xml.getTagValue(page, "title");
                             String definition = text.getDefinition(article);
-
                             //TODO: postevaluate
                             if (evaluateDefinition(definition)) {
                                 if (verbose) {
@@ -141,24 +160,35 @@ public class wikiFileDumpParser {
                     }
                 }
                 file.close();
-            } catch (Exception e) {
+            } catch (NullPointerException | IOException e) {
                 e.printStackTrace();
             }
         }
 
+        /**
+         * reads next line from cursor and converts it to UTF-8
+         * @return next line as string
+         */
         private String readLineUTF() {
+            //set buffersize
             int buffersize = 128;
             try {
                 String line = "";
+                //remember position
                 long pos = file.getChannel().position();
                 do {
                     byte[] charbuffer = new byte[buffersize];
                     for (int i = 0; i < buffersize; i++) {
+                        //fill charbuffer
                         charbuffer[i] = (byte) file.read();
                     }
+                    //add buffer to line
                     line += new String(charbuffer, "UTF-8");
+                    //repeat until linebreak reched
                 } while (!line.contains("\n"));
+                //shorten line until lineend
                 line = line.substring(0, line.indexOf("\n"));
+                //set cursor back to lineend
                 file.getChannel().position(pos + line.getBytes().length + 1); //schneller als file.seek ..
                 return line;
             } catch (Exception e) {
@@ -167,6 +197,11 @@ public class wikiFileDumpParser {
             }
         }
 
+        /**
+         * cheacks if definition has the right length
+         * @param definition definition
+         * @return returns true if the definition is valid and false if it isn't
+         */
         private boolean evaluateDefinition(String definition) {
             if (definition.length() < 30 || definition.length() > 3000) {
                 errors++;
@@ -176,6 +211,5 @@ public class wikiFileDumpParser {
                 return true;
             }
         }
-
     }
 }
